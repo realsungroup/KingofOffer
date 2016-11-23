c9ccb29252affa27df617794d7db76952b82f235
fujia <5211666@qq.com>
2016-11-23 18:32:46 +0800

    crate

--- source/app/main/viewmodels/shell.js
+++ source/app/main/viewmodels/shell.js
@@ -9,7 +9,7 @@
         activate: function () {
             router.map([
                 { route: '', title:'欢迎', moduleId: 'main/viewmodels/welcome', nav: true },
-                { route: 'orderrpt*details',title:'统计', moduleId: 'orderrpt/index', nav: true },
+                { route: 'orderrpt*details',title:'管理', moduleId: 'orderrpt/index', nav: true },
                 { route: 'update',title:'重新登入', moduleId: 'main/viewmodels/update', nav: true }
             ]).buildNavigationModel();
            
