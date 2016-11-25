7caa0e9543eb85cd2c07e36ce51d0c942ef1530b
fujia <esc@fujiadeMacBook-Air.local>
2016-11-25 17:55:21 +0800

    ggwp

--- release/app/orderrpt/viewmodels/basicmenu.js
+++ release/app/orderrpt/viewmodels/basicmenu.js
@@ -2,6 +2,7 @@ define(['durandal/app','knockout','plugins/router','plugins/dialog','calendar/fu
 
     return {
         activate:function(){},
+        mlist:ko.observableArray([]),
         attached:function(){
             mini.parse();  
             baseUrl=appConfig.app.baseUrl;
@@ -9,9 +10,33 @@ define(['durandal/app','knockout','plugins/router','plugins/dialog','calendar/fu
             var user  = appConfig.app.user;
             var dbs=new dbHelper(baseUrl,user,ucode);
             var cpnid=appConfig.app.cpnid;
-            var resid=appConfig.app.resid
-            
+            var resid=appConfig.app.resid;
+            var cmswhere="",cpn="";
 
+            menuIndex=function(self,cpn){
+                if(cpn){
+                    cmswhere=cpn;
+                }
+                // if(C3_530883695470==undefined){
+                //     cmswhere="";
+                // }else if(C3_530883695470==undefined){
+                //     cmswhere="";
+                // }else if(C3_530883695470==undefined){
+                //     cmswhere="";
+                // }else if(C3_530883695470==undefined){
+                //     cmswhere="";
+                // }else{
+                //     cmswhere="";
+                //}
+                dbs.dbGetdata(cpnid,0,cmswhere,fnSuccess,null,fnhttperror);
+                function fnSuccess(data){
+                    self.mlist(data);
+                }
+                function fnhttperror(jqXHR, textStatus, errorThrown){
+                    console.log(jqXHR);
+                }
+            }
+            menuIndex(this);
         //     var fileupload = mini.get("fileupload1");
         //     fileupload.setUploadUrl(appConfig.app.uploadFileUrl+appConfig.app.uppath+appConfig.app.httppath);
         //     var imgfield=mini.get('imgurl');
@@ -23,29 +48,33 @@ define(['durandal/app','knockout','plugins/router','plugins/dialog','calendar/fu
         //     }
 
 
-            saveClick=function(){
-                    mini.parse();
-                    var form = new mini.Form("form");
-                    var o =  new mini.Form("form").getData();
-                    form.validate(); 
-                    if (form.isValid() == false) return;
-                    o._id=1;
-                    o._state="added";
-                    var json = mini.encode([o]);
-                    console.log(json);
-                    dbs.dbSavedata(resid,0,json,dataSaved,fnerror,fnhttperror);
+            // saveClick=function(){
+            //         mini.parse();
+            //         var form = new mini.Form("form");
+            //         var o =  new mini.Form("form").getData();
+            //         form.validate(); 
+            //         if (form.isValid() == false) return;
+            //         o._id=1;
+            //         o._state="added";
+            //         var json = mini.encode([o]);
+            //         console.log(json);
+            //         dbs.dbSavedata(resid,0,json,dataSaved,fnerror,fnhttperror);
                     
-                    function dataSaved(text){
-                        dialog.showMessage('<h1>添加成功</h1>','菜品新增',['返回'],true);
-                    }
-                    function fnerror(text){
-                        dialog.showMessage(text.message,'添加失败',['返回'],true);
-                    }
-                    function fnhttperror(jqXHR, textStatus, errorThrown){
-                        dialog.showMessage('error','菜品新增',['返回'],true);
-                    }
+            //         function dataSaved(text){
+            //             dialog.showMessage('<h1>添加成功</h1>','菜品新增',['返回'],true);
+            //         }
+            //         function fnerror(text){
+            //             dialog.showMessage(text.message,'添加失败',['返回'],true);
+            //         }
+            //         function fnhttperror(jqXHR, textStatus, errorThrown){
+            //             dialog.showMessage('error','菜品新增',['返回'],true);
+            //         }
 
-            }  
-        }
+            // }  
+        },
+        cpnClick:function(cpn){
+            console.log(cpn);
+            //cpmenu(this,cpn);
+        },
     }
 })
