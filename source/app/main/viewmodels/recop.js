7c018b304e13aa646b5986b3a3b61c04bf25810d
fujia <5211666@qq.com>
2016-12-16 10:55:33 +0800

    gq

--- source/app/main/viewmodels/recop.js
+++ source/app/main/viewmodels/recop.js
@@ -25,28 +25,30 @@ define(['durandal/app','knockout','plugins/router','plugins/dialog'], function (
         }
         search =  function() {
             newData=[];
-            var skey = $('#searchBox').val();
+            var skey = mini.getbyName('searchBox').value;
+            console.log(sData[0]);
             for(var i=0,a=0;i<sData.length;i++){
                 if(sData[i].C3_522691671297.indexOf(skey)>=0
-                    || sData[i].C3_522691669347.indexOf(skey)>=0
-                    || sData[i].C3_522691670315.indexOf(skey)>=0
-                    || sData[i].C3_522691669613.indexOf(skey)>=0
-                    || sData[i].C3_522691669878.indexOf(skey)>=0
-                    || sData[i].C3_522691670096.indexOf(skey)>=0)
+                || sData[i].C3_522691669347.indexOf(skey)>=0
+                || sData[i].C3_522691670315.indexOf(skey)>=0
+                || sData[i].C3_522691669613.indexOf(skey)>=0
+                || sData[i].C3_522691669878.indexOf(skey)>=0
+                || sData[i].C3_522691670096.indexOf(skey)>=0
+                || sData[i].C3_522691670814.indexOf(skey)>=0)
                 newData[a++] = sData[i];
             }
             me.recopList(newData);
         };
-        $('#searchBox').keydown(function(evt) {
+        $('#searchBox').keydown(function(event) {
             if (event.keyCode == "13") {//keyCode=13是回车键
-                $('#searchBtn').click();
                 event.preventDefault();
+                $('#searchBtn').click();
             }
         });
     };
-    recop.prototype.cancel = function() {
+    recop.prototype.cancel = function(){
         dialog.close(this);
-        };
+    };
 
     recop.show = function(){
         return dialog.show(new recop());
